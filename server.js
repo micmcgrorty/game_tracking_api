827cc8d716371d07b4384313ae097df3c231ce62
Michael McGrorty <mmcgrorty@vidatec.com>
2021-03-10 13:48:57 +0000

    fix: add cors package

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const cors = require('cors');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const corsOptions = {
+  origin: process.env.APP_ORIGIN
+};
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,9 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', process.env.APP_ORIGIN);
-  });
+  app.use(cors(corsOptions));
 }
 
 app.use('/search', require('./api/routes/search-router'));
