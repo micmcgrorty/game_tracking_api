0e53b97ed8e047b575c175dfec08d70ce9dbbbea
Michael McGrorty <mmcgrorty@vidatec.com>
2021-03-11 22:51:53 +0000

    feat: add popular games and new releases routes

--- api/routes/game-router.js
+++ api/routes/game-router.js
@@ -4,6 +4,44 @@ const igdb = require('igdb-api-node').default;
 
 module.exports = router;
 
+router.get('/popular', async (req, res) => {
+  const response = await igdb(
+    process.env.IGDB_CLIENT_ID,
+    process.env.IGDB_ACCESS_TOKEN
+  )
+    .fields(['*', 'cover.*'])
+    .limit('9')
+    .sort('rating', 'desc')
+    .where(
+      `first_release_date != null &
+      first_release_date < ${new Date().getTime()} & 
+      rating != null &
+      rating_count > 250 &
+      category = 0`
+    )
+    .request('/games');
+
+  res.send(response.data);
+});
+
+router.get('/new-releases', async (req, res) => {
+  const response = await igdb(
+    process.env.IGDB_CLIENT_ID,
+    process.env.IGDB_ACCESS_TOKEN
+  )
+    .fields(['*', 'cover.*'])
+    .limit('9')
+    .sort('first_release_date', 'desc')
+    .where(
+      `first_release_date != null &
+      first_release_date < ${new Date().getTime()} &
+      category = 0`
+    )
+    .request('/games');
+
+  res.send(response.data);
+});
+
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
